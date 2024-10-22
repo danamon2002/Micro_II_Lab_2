411081e144b8874861b60c6f99415bb5361a90c1
Dana Maloney <danamon2002@gmail.com>
2024-10-22 14:26:49 -0400

    Initial Commit

---
+++ Micro_II_Lab_2.ino
@@ -0,0 +1,12 @@
+//Authors: Dana Maloney, Thomas Moeller
+// Micro 2 FA24 Lab 2
+
+void setup() {
+  // Setup code
+  Serial.begin(9600);
+}
+
+void loop() {
+  // Main Loop code
+  int x_axis = analogRead()
+}
